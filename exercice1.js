74ef6436a3fce86d582d5d8f2465d6de3ea6d15a
sstienface <s.stien@fondationface.org>
2018-10-19 11:49:28 +0200

    Initial Commit

---
+++ exercice1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+var personne = {
+    "prenom" : "James",
+    "nom" : "Bond"
+};
+
+/* Afficher ci dessous la propriété nom de notre objet dans un alert */
+
+alert();
+
+
---
+++ exercice2/exercice2.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+var personne = {
+    "prenom" : "James",
+    "nom" : "Bond",
+           :
+};
+
+/* Afficher ci dessous la propriété metier de notre objet dans un alert */
+
+alert();
+
+
